12b33a56ce6db96820e83d449e6ade5654b119f2
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-07 11:58:55 -0800

    Finish stylizing nav bar with css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,13 @@ function Header() {
     return (
         <header className = "top-header">
             <nav className = "navigation">
-                <img src="./react-logo.png"  className = "nav-logo" />
-                <h2 className = "nav-title">ReactFacts</h2>
-                <span className = "nav-right">React Course - Project 1</span>
+                <div className = 'left-side'>
+                        <img src="./react-logo.png"  className = "nav-logo" />
+                        <h2 className = "nav-title">ReactFacts</h2>
+                </div>
+               <div className = 'right-side'>
+                    <span className = "nav-right">React Course - Project 1</span>
+               </div>
             </nav>
         </header>
     )
