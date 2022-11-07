187d1d22ef395fa8388b1c3bdad4d3a91726fca4
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-07 12:32:56 -0800

    Finish stylizing main content

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -3,14 +3,17 @@ import React, { Component } from "react"
 function MainContent () {
     return (
         <div className="mainBody">
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of entreprise apps, including mobile apps.</li>
-            </ul>
+            <div className="centerContent">
+                <h1 className = "main-title">Fun facts about React</h1>
+                <ul className = "list">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on Github</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of entreprise apps, including mobile apps.</li>
+                </ul>
+            </div>
+
         </div> 
     )
 
